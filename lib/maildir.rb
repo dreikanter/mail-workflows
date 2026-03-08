b941ba4b898bb85b29aba6830ceec988dc1bece6
Alex Musayev <alex.musayev@gmail.com>
2026-03-08 11:04:39 +0100

    Split MaildirStore into its own file, make internal methods private

--- lib/maildir.rb
+++ lib/maildir.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require "fileutils"
-require "yaml"
 
 module MailWorkflows
   # Represents a single Maildir folder (e.g., ~/.mail-workflows/mail/personal/INBOX).
@@ -67,58 +66,6 @@ module MailWorkflows
       dest = File.join(dest_dir, File.basename(filepath))
       File.rename(filepath, dest)
       dest
-    end
-  end
-
-  # Manages all Maildir folders across configured accounts.
-  # Reads account configuration from accounts.yml and provides
-  # access to individual Maildir instances.
-  class MaildirStore
-    attr_reader :home
-
-    def initialize(home = nil)
-      @home = home || ENV.fetch("MAIL_WORKFLOWS_HOME", File.expand_path("~/.mail-workflows"))
-    end
-
-    def config
-      @config ||= YAML.load_file(accounts_path, permitted_classes: [Symbol])
-    end
-
-    def accounts
-      config.fetch("accounts", {})
-    end
-
-    def accounts_path
-      File.join(@home, "accounts.yml")
-    end
-
-    def mail_path
-      File.join(@home, "mail")
-    end
-
-    # Returns array of Maildir instances for all configured account/folder pairs.
-    def maildirs
-      accounts.flat_map do |name, account|
-        folders = account.fetch("folders", ["INBOX"])
-        folders.map do |folder|
-          Maildir.new(File.join(mail_path, name, folder))
-        end
-      end
-    end
-
-    # Create all Maildir directory structures.
-    def ensure_all_dirs
-      maildirs.each(&:ensure_dirs)
-    end
-
-    # Iterate over new (unprocessed) messages across all maildirs.
-    # Yields [filepath, maildir] pairs.
-    def each_new_message
-      return enum_for(:each_new_message) unless block_given?
-
-      maildirs.each do |maildir|
-        maildir.new_messages.each { |msg| yield msg, maildir }
-      end
     end
   end
 end
